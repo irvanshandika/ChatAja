127916ac1fa00d04b071ccacbfa990ea57fffdf0
irvanshandika <shandikamuhammadirvan@gmail.com>
2024-11-01 11:06:22 +0700

    Llama AI Chat

---
+++ app/api/chat/route.ts
@@ -0,0 +1,18 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+const fireworks = new OpenAI({
+  apiKey: process.env.FIREWORKS_API_KEY || "",
+  baseURL: "https://api.fireworks.ai/inference/v1",
+});
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+  const response = await fireworks.chat.completions.create({
+    model: "accounts/fireworks/models/llama-v3p2-11b-vision-instruct",
+    stream: true,
+    max_tokens: 1000,
+    messages,
+  });
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Chat from "@/src/components/chat";
+
+function Home() {
+  return (
+    <>
+      <Chat />
+    </>
+  );
+}
+
+export default Home;
