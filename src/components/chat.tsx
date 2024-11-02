27fdc556b07044bf4635e3edaf9e6f1ca721e9a5
irvanshandika <shandikamuhammadirvan@gmail.com>
2024-11-02 21:23:41 +0700

    Fix responsive UI

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -11,18 +11,18 @@ export default function Chat() {
       <header className="text-center">
         <h1 className="text-xl">Llama AI</h1>
       </header>
-      <div className="flex flex-col justify-between w-full max-w-md mx-auto stretch">
+      <div className="flex flex-col justify-between w-full max-w-md mx-auto">
         <div className="flex-grow overflow-y-auto">
           {messages.map((m) => (
             <div key={m.id} className="whitespace-pre-wrap flex gap-x-2">
               {m.role === "user" ? (
                 <>
-                  <UserIcon className="w-6 h-6 inline-block" />
+                  <UserIcon className="w-6 h-6 inline-block flex-shrink-0" />
                   <span>:</span>
                 </>
               ) : (
                 <>
-                  <MetaIcon className="w-6 h-6 inline-block" />
+                  <MetaIcon className="w-6 h-6 inline-block flex-shrink-0" />
                   <span>:</span>
                 </>
               )}
@@ -30,8 +30,13 @@ export default function Chat() {
             </div>
           ))}
         </div>
-        <form onSubmit={handleSubmit}>
-          <input className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl" value={input} placeholder="Say something..." onChange={handleInputChange} />
+        <form onSubmit={handleSubmit} className="mt-4">
+          <input
+            className="w-full p-2 border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
         </form>
       </div>
     </div>
